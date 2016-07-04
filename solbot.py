c8796771adcea675cc6c3485a52c639a973d7720
Johnathon Ludeman <jludeman@kabaminc.com>
2016-07-03 17:45:20 -0700

    initial test

---
+++ solbot.py
@@ -0,0 +1,30 @@
+import discord
+import asyncio
+import json
+
+client = discord.Client()
+
+@client.event
+async def on_read():
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
+
+@client.event
+async def on_message(message):
+	if message.content.startswith('!test'):
+		counter = 0
+		tmp = await client.send_message(message.channel, 'Calculating messages...')
+		async for log in client.logs_from(message.channel, limit=100)
+			if log.author == message.author:
+				counter += 1
+
+		await client.edit_message(tmp, 'You have {} message.'.format(counter))
+	elif message.content.startswith('!sleep'):
+		await asyncio.sleep(5)
+		await client.send_message(message.channel, 'Done sleeping')
+
+with open('token') as token_file:
+	token = json.load(token_file)['token']
+client.run(token)
