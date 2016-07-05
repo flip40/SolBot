aff97c4816ce05b2bf5ec9df42508b02941e0681
John Ludeman <john@ludeman.com>
2016-07-04 17:58:35 -0700

    add sleep functionality, get update status

--- solbot.py
+++ solbot.py
@@ -6,6 +6,7 @@ import sys
 from git import Git
 
 client = discord.Client()
+sleeping = False
 
 @client.event
 async def on_ready():
@@ -13,26 +14,39 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.content == '!test':
-		counter = 0
-		tmp = await client.send_message(message.channel, 'Calculating messages...')
-		async for log in client.logs_from(message.channel, limit=100):
-			if log.author == message.author:
-				counter += 1
+	if sleeping:
+		if message.content == '!wake':
+			sleeping = False
+			await client.send_message(message.channel, 'SolBot online!')
+	else:
+		if message.content == '!test':
+			counter = 0
+			tmp = await client.send_message(message.channel, 'Calculating messages...')
+			async for log in client.logs_from(message.channel, limit=100):
+				if log.author == message.author:
+					counter += 1
 
-		await client.edit_message(tmp, 'You have {} message.'.format(counter))
-	elif message.content == '!sleep':
-		await asyncio.sleep(5)
-		await client.send_message(message.channel, 'Done sleeping')
-	elif message.content == '!update':
-		g = Git(os.path.dirname(os.path.abspath(__file__)))
-		tmp = await client.send_message(message.channel, 'Pulling new code...')
-		g.pull()
-		await client.edit_message(tmp, 'Code pulled. Restarting...')
-		client.logout()
-		os.execv(sys.executable, ['python3.5'] + sys.argv)
-	elif message.content == '!gitstatus':
-		await client.send_message(message.channel, 'Success!')
+			await client.edit_message(tmp, 'You have {} message.'.format(counter))
+		elif message.content == '!sleep':
+			sleeping = True
+			await client.send_message(message.channel, 'Going to sleep...')
+		elif message.content == '!update':
+			g = Git(os.path.dirname(os.path.abspath(__file__)))
+			tmp = await client.send_message(message.channel, 'Pulling new code...')
+			g.pull()
+			await client.edit_message(tmp, 'Code pulled. Restarting...')
+			client.logout()
+			os.execv(sys.executable, ['python3.5'] + sys.argv)
+		elif message.content == '!gitstatus':
+			g = Git(os.path.dirname(os.path.abspath(__file__)))
+			tmp = await client.send_message(message.channel, 'Checking status...')
+			g.fetch()
+			p = re.compile('Your branch is.*by (\d+) commits.*')
+			m = p.match(g.status())
+			if m:
+				await client.edit_message(tmp, 'I am behind by {} commits'.format(m.group(1)))
+			else:
+				await client.edit_message(tmp, 'I am up to date!')
 
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'token')) as token_file:
 	token = json.load(token_file)['token']
